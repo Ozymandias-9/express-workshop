d2ed7df3b2a4386bd8084def6ff9ef5c39627a35
Oscar Melchor <galac.9173@gmail.com>
2020-10-07 22:17:29 -0500

    program updated

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 //Importa la libreria de express
 const app = express();
 //Obtener instancia, llama al constructor de express, acceso a express
-
+const { pokemon } = require('./pokedex.json');
 /*
 Verbos HTTP:
 recurso: Registro en alguna base de datos
@@ -14,9 +14,7 @@ DELETE: Eliminar un recurso
 */
 
 app.get("/", (req,res,next) =>{
-    res.status(200);
-    res.send("Hola Mundo");
-    res.send("Bienvenido.");
+    res.send("Bienvenido al Pokedex");
 })
 
 //Primer parametro, URL que va a recibir 
@@ -27,7 +25,38 @@ res: La respuesta que vamos a dar (response)
 next: ...
  */
 
-app.listen(3000, () => {
+app.get("/pokemon/all",(req,res,next) => {
+//    console.log(req.params.name);
+    res.status(200);
+    res.send(pokemon);
+});
+
+app.get('/pokemon/:id([0-9]{1,3})', (req,res,next) => {
+    const id = req.params.id-1;
+    if (id >= 0 && id <= 150){
+    res.status(200);
+    res.send(pokemon[req.params.id - 1]);
+    }
+    else {
+        res.status(404);
+        res.send("Pokémon no encontrado.")
+    }
+})
+
+app.get('/pokemon/:name', (req, res, next) => {
+    const name = req.params.name;
+    for(i = 0;i < pokemon.length; i++) {
+        if (pokemon[i].name == name){
+            res.status(200);
+            res.send(pokemon[i]);
+
+        }
+        res.status(404);
+        res.send("Pokémon no encontrado.");
+    }
+});
+
+app.listen(process.env.PORT || 3000, () => {
 console.log("Server is running...");
 });
 //Primer parametro, puerto del servidor
