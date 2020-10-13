59e9fa3cab0472a05c7b7f73283b9278a5e0aa5e
Oscar Melchor <galac.9173@gmail.com>
2020-10-12 19:10:22 -0500

    program updated

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 //Importa la libreria de express
 const app = express();
 //Obtener instancia, llama al constructor de express, acceso a express
 const { pokemon } = require('./pokedex.json');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({  extended: true}));
 /*
 Verbos HTTP:
 recurso: Registro en alguna base de datos
 GET: Obtener un recurso
-POST: Guardar o publicar algo, postear algo
-PATCH: Actualización de un dato de un recurso
+POST: Almacenar, crear recursos
 PUT: Modificar todos los recursos
+PATCH: Actualización de un dato de un recurso
 DELETE: Eliminar un recurso
 */
 
@@ -25,36 +29,40 @@ res: La respuesta que vamos a dar (response)
 next: ...
  */
 
-app.get("/pokemon/all",(req,res,next) => {
+
+app.post("/pokemon", (req,res,next) => {
+    return res.status(200).send(req.body);
+})
+
+app.get("/pokemon",(req,res,next) => {
 //    console.log(req.params.name);
-    res.status(200);
-    res.send(pokemon);
+    res.status(200).send(pokemon);
 });
+//res.status(200).send(cosa)
 
 app.get('/pokemon/:id([0-9]{1,3})', (req,res,next) => {
     const id = req.params.id-1;
-    if (id >= 0 && id <= 150){
-    res.status(200);
-    res.send(pokemon[req.params.id - 1]);
-    }
-    else {
-        res.status(404);
-        res.send("Pokémon no encontrado.")
-    }
-})
-
-app.get('/pokemon/:name', (req, res, next) => {
-    const name = req.params.name;
-    for(i = 0;i < pokemon.length; i++) {
-        if (pokemon[i].name == name){
-            res.status(200);
-            res.send(pokemon[i]);
-
-        }
-        res.status(404);
-        res.send("Pokémon no encontrado.");
-    }
+    (id >= 0 && id <= 150) ? 
+        res.status(200).send(pokemon[req.params.id - 1]) :
+        res.status(404).send("Pokémon no encontrado.");
+    
 });
+
+app.get('/pokemon/:name([A-Za-z]+)', (req, res, next) => {
+    const name = req.params.name;
+
+    const pk = pokemon.filter((p)=>{
+            return (p.name.toUpperCase() == name.toUpperCase()) && p;
+        });
+
+        console.log(pk);
+
+        (pk.length>0) ? 
+        res.status(200).send(pk) : 
+        res.status(404).send("Pokémon no encontrado.");
+    
+});
+
 
 app.listen(process.env.PORT || 3000, () => {
 console.log("Server is running...");
