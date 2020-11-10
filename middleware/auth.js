85f7649dc0ca09bc41ec5bf0e373ae67b9da6f77
Oscar Melchor <galac.9173@gmail.com>
2020-11-10 07:31:56 -0600

    JWT working, middlewares

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
     try {
-        const token = req.header.authorization.split(" ")[1];
+        const token = req.headers.authorization.split(" ")[1];
         const decoded = jwt.verify(token, "debugkey");
         req.user = decoded;
         next();
@@ -11,3 +11,4 @@ module.exports = (req, res, next) => {
         return res.status(401).json({code: 401, message: "No tienes permiso :("});
     }
 }
+
